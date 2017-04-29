bdfeae706c84989a789579d9c05fe7dcb9f20659
hgiasac <hgiasac@gmail.com>
2017-04-29 10:31:49 +0700

    update html dropdownlist

--- grid/DataColumn.php
+++ grid/DataColumn.php
@@ -2,6 +2,8 @@
 
 namespace bulma\grid;
 
+use bulma\helpers\Html;
+
 class DataColumn extends \yii\grid\DataColumn
 {
 
@@ -12,4 +14,37 @@ class DataColumn extends \yii\grid\DataColumn
      * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
      */
     public $filterInputOptions = ['class' => 'input', 'id' => null];
+
+    /**
+ * @inheritdoc
+ */
+protected function renderFilterCellContent()
+{
+    if (is_string($this->filter)) {
+        return $this->filter;
+    }
+    $model = $this->grid->filterModel;
+    if ($this->filter !== false && $model instanceof Model && $this->attribute !== null && $model->isAttributeActive($this->attribute)) {
+        if ($model->hasErrors($this->attribute)) {
+            Html::addCssClass($this->filterOptions, 'is-danger');
+            $error = ' ' . Html::error($model, $this->attribute, $this->grid->filterErrorOptions);
+        } else {
+            $error = '';
+        }
+        if (is_array($this->filter)) {
+            $options = array_merge(['prompt' => ''], $this->filterInputOptions);
+            return Html::activeDropDownList($model, $this->attribute, $this->filter, $options) . $error;
+        } elseif ($this->format === 'boolean') {
+            $options = array_merge(['prompt' => ''], $this->filterInputOptions);
+            return Html::activeDropDownList($model, $this->attribute, [
+                $this->grid->formatter->booleanFormat[0],
+                $this->grid->formatter->booleanFormat[1],
+            ], $options) . $error;
+        } else {
+            return Html::activeTextInput($model, $this->attribute, $this->filterInputOptions) . $error;
+        }
+    } else {
+        return parent::renderFilterCellContent();
+    }
+}
 }
