087c8c510c333cede7ab69649f301f2343653f2a
hgiasac <hgiasac@gmail.com>
2017-04-30 00:08:22 +0700

    update ActiveField

--- widgets/ActiveField.php
+++ widgets/ActiveField.php
@@ -2,6 +2,8 @@
 
 namespace bulma\widgets;
 
+use bulma\helpers\Html;
+
 /**
  * ActiveField represents a form input field within an [[ActiveForm]].
  *
@@ -70,4 +72,78 @@ class ActiveField extends \yii\widgets\ActiveField
      * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
      */
     public $hintOptions = ['class' => 'help'];
+
+
+    /**
+     * Renders a drop-down list.
+     * The selection of the drop-down list is taken from the value of the model attribute.
+     * @param array $items the option data items. The array keys are option values, and the array values
+     * are the corresponding option labels. The array can also be nested (i.e. some array values are arrays too).
+     * For each sub-array, an option group will be generated whose label is the key associated with the sub-array.
+     * If you have a list of data models, you may convert them into the format described above using
+     * [[ArrayHelper::map()]].
+     *
+     * Note, the values and labels will be automatically HTML-encoded by this method, and the blank spaces in
+     * the labels will also be HTML-encoded.
+     * @param array $options the tag options in terms of name-value pairs.
+     *
+     * For the list of available options please refer to the `$options` parameter of [[\yii\helpers\Html::activeDropDownList()]].
+     *
+     * If you set a custom `id` for the input element, you may need to adjust the [[$selectors]] accordingly.
+     *
+     * @return $this the field object itself.
+     */
+    public function dropDownList($items, $options = [])
+    {
+        $options = array_merge($this->inputOptions, $options);
+        $this->addAriaAttributes($options);
+        $this->adjustLabelFor($options);
+        $this->parts['{input}'] = Html::activeDropDownList($this->model, $this->attribute, $items, $options);
+        return $this;
+    }
+
+    /**
+     * Renders a text input.
+     * This method will generate the `name` and `value` tag attributes automatically for the model attribute
+     * unless they are explicitly specified in `$options`.
+     * @param array $options the tag options in terms of name-value pairs. These will be rendered as
+     * the attributes of the resulting tag. The values will be HTML-encoded using [[Html::encode()]].
+     *
+     * The following special options are recognized:
+     *
+     * - `maxlength`: int|bool, when `maxlength` is set `true` and the model attribute is validated
+     *   by a string validator, the `maxlength` option will take the value of [[\yii\validators\StringValidator::max]].
+     *   This is available since version 2.0.3.
+     *
+     * Note that if you set a custom `id` for the input element, you may need to adjust the value of [[selectors]] accordingly.
+     *
+     * @return $this the field object itself.
+     */
+    public function textInput($options = [])
+    {
+        $options = array_merge($this->inputOptions, $options);
+        $this->addAriaAttributes($options);
+        $this->adjustLabelFor($options);
+        $this->parts['{input}'] = Html::activeTextInput($this->model, $this->attribute, $options);
+        return $this;
+    }
+
+    /**
+     * Renders an input tag.
+     * @param string $type the input type (e.g. `text`, `password`)
+     * @param array $options the tag options in terms of name-value pairs. These will be rendered as
+     * the attributes of the resulting tag. The values will be HTML-encoded using [[Html::encode()]].
+     *
+     * If you set a custom `id` for the input element, you may need to adjust the [[$selectors]] accordingly.
+     *
+     * @return $this the field object itself.
+     */
+    public function input($type, $options = [])
+    {
+        $options = array_merge($this->inputOptions, $options);
+        $this->addAriaAttributes($options);
+        $this->adjustLabelFor($options);
+        $this->parts['{input}'] = Html::activeInput($type, $this->model, $this->attribute, $options);
+        return $this;
+    }
 }
